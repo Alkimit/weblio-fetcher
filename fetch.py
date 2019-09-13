c0bc2e820edd098b850a6295d816e7034b4287be
rekkuso <jnrodewald99@gmail.com>
2019-09-13 12:11:36 +0000

    Fixed separation + parsing + export

--- fetch.py
+++ fetch.py
@@ -2,63 +2,46 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
-def fetchSource(vocab):
+def getDefinition(vocab):
 	# Grab word from user and fetch site source
 	headers = {'User-Agent': 'Mozilla/5.0 | github.com/rekkuso'}
-	source = requests.get('https://www.weblio.jp/content/' + vocab, headers=headers)
+	source = requests.get('https://www.weblio.jp/content/' + vocab.strip(), headers=headers)
 	time.sleep(2)
+
 	# Check response
 	print("Return code: " + str(source.status_code))
 	if(source.status_code != 200 or source.text == ""):
 		print("Error fetching HTML.")
 		return ConnectionAbortedError
 
+	# ~soup~
 	soup = BeautifulSoup(source.text, 'lxml')
 
-	# Find definitions part of website source
+	# Find definitions block in website source
 	results = soup.find_all('div', attrs={'class':'kiji'})
 
-	# Final string
-	resString = ""
-	# Grab each definition and concatenate
-	rtemp = ""
-	for r in results:
-		if rtemp == r.parent.text:
-			continue
-		resString += r.parent.text
-		rtemp = r.parent.text
+	# Create final result list and start with word itself;
+	# The final list will have the word in front and its definitions afterwards
+	final = list()
+	final.append(vocab)
+	final[0] = final[0].rstrip()
 
+	# Separate multiple definitions; WIP
+	# Char 9312 = circled number; if one is present, its a multi-def.
+	if results[0].text.strip().count(chr(9312)) > 0:
+		cBuffer = ""
+		for c in results[0].text.strip():
+			if c >= chr(9312) and c <= chr(9331):
+				final.append(cBuffer.strip())
+				cBuffer = ""
+				cBuffer += c
+				continue
+			cBuffer += c
 
-	# Strip ends of whitespace
-	resString = resString.strip()
+	# Only one definition: Add the result that was found
+	else:
+		final.append(results[0].text.strip())
 
-	#print("/*------------------")
-	#print(resString)
-	#print("-------------------*/")
-
-	if(len(resString) < 1):
-		print("No definition found.")
-		return ConnectionAbortedError
-
-	# Buffer for definition separation
-	buf = ""
-	# List of final results
-	definitions = list()
-
-	for i in range(0, len(resString)):
-		# 9312 = circled numbers; If one is present, there are multiple definitions
-		if resString.count(chr(9312)) > 0:
-			# Check for subsequent number symbols and add a string to the list for each definition
-			if ord(resString[i]) >= 9312 and ord(resString[i]) <= 9331:
-				if(buf != ""):
-					definitions.append(buf)
-					buf = ""
-			buf += resString[i]
-			continue
-		# Only one definition
-		buf += resString[i]
-	# Return all definitions
-	definitions.append(buf)
-	return definitions
+	return final
 
 
--- main.py
+++ main.py
@@ -4,21 +4,15 @@ import csv
 if __name__ == "__main__":
     # Read word list and parse
     vocabfile = open("words.txt", "r")
-    definitions = list()
-    for vocab in vocabfile:
 
-        result = fetch.fetchSource(vocab)
-        try:
-            # TODO: Fix inconsistent/empty returns from website
-            definitions.extend(result)
-        except:
-            print("Error parsing " + vocab)
-            continue
-        definitions.append("\n")
-        print(result)
-
-    # CSV Export
+    # Open CSV file
     with open('test_output.csv', mode='w') as test_file:
-        test_writer = csv.writer(test_file, dialect='excel', delimiter=',', quoting=csv.QUOTE_NONE, escapechar=' ')
-        test_writer.writerow(definitions)
+        test_writer = csv.writer(test_file, dialect='excel', escapechar=' ', quoting=csv.QUOTE_NONE)
+        for vocab in vocabfile:
+            # Returns string/list of definitions for each word in the list (+debug print)
+            result = fetch.getDefinition(vocab)
+            print(result)
+
+            # CSV Export
+            test_writer.writerow(result)
 
